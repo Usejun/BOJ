7525b55966191421bb955c61ed30a2e22fc88da1
Usein_ <84970153+UseinUse@users.noreply.github.com>
2023-06-11 18:40:19 +0900

    Update Program.cs

--- 2588번 곱셈/Program.cs
+++ 2588번 곱셈/Program.cs
@@ -1,7 +1,25 @@
-﻿string num1 = Console.ReadLine();
-string num2 = Console.ReadLine();
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Text.RegularExpressions;
 
-Console.WriteLine(int.Parse(num1) * Convert.ToInt32(num2[2] - '0'));
-Console.WriteLine(int.Parse(num1) * Convert.ToInt32(num2[1] - '0'));
-Console.WriteLine(int.Parse(num1) * Convert.ToInt32(num2[0] - '0'));
-Console.WriteLine(int.Parse(num1) * int.Parse(num2));
+namespace Boj
+{
+    internal class Program
+    {     
+        static void Main(string[] args)
+        {
+            string num1 = Console.ReadLine();
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine(int.Parse(num1) * Convert.ToInt32(num2[2] - '0'));
+            Console.WriteLine(int.Parse(num1) * Convert.ToInt32(num2[1] - '0'));
+            Console.WriteLine(int.Parse(num1) * Convert.ToInt32(num2[0] - '0'));
+            Console.WriteLine(int.Parse(num1) * int.Parse(num2));
+        }
+    }    
+}
