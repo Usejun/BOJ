52b7e214f2b7c1a885dd76ad25036047bf047308
Usein_ <leeyj7709@gmail.com>
2023-06-11 18:34:15 +0900

    Create Program.cs

---
+++ 2588번 곱셈/Program.cs
@@ -0,0 +1,7 @@
+﻿string num1 = Console.ReadLine();
+string num2 = Console.ReadLine();
+
+Console.WriteLine(int.Parse(num1) * Convert.ToInt32(num2[2] - '0'));
+Console.WriteLine(int.Parse(num1) * Convert.ToInt32(num2[1] - '0'));
+Console.WriteLine(int.Parse(num1) * Convert.ToInt32(num2[0] - '0'));
+Console.WriteLine(int.Parse(num1) * int.Parse(num2));
