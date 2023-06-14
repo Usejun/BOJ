13d2888fd851b562b4c8e2eaad91227295040bf3
Usein_ <84970153+UseinUse@users.noreply.github.com>
2023-06-14 20:24:36 +0900

    Update Program.cs

--- 4344번/Program.cs
+++ 4344번/Program.cs
@@ -1,7 +1,26 @@
-int n = int.Parse(Console.ReadLine());
-for(int i = 0; i < n; i++)
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Boj
 {
-    var input = Console.ReadLine().Split().Select(double.Parse).ToList();
-    var score = input.Skip(1).ToList();             
-    Console.WriteLine($"{score.Where(x => x > score.Average()).Count()*100/input[0]:0.000}%");
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split().Select(double.Parse).ToList();
+                var score = input.Skip(1).ToList();             
+                Console.WriteLine($"{score.Where(x => x > score.Average()).Count()*100/input[0]:0.000}%");
+            }
+        }
+    }
 }
