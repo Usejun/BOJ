c7b25d5ec9f392dd4b6f9398f7eba2de05f8d28e
Usejun <84970153+Usejun@users.noreply.github.com>
2023-07-12 14:28:19 +0900

    Create Program.cs

---
+++ 1015번/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Boj
+{
+    internal class Program
+    {     
+        static void Main(string[] args)            
+        {
+            int n = int.Parse(Console.ReadLine());
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var answer = new int[n];
+            int count = 0;
+            for (int i = 0; i < 1001; i++)            
+                for (int j = 0; j < array.Length; j++)
+                    if (array[j] == i)
+                        answer[j] = count++;
+            Console.WriteLine(string.Join(" ", answer));
+        }
+    }
+}
+    
