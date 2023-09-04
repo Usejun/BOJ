06f1745a5b381c4248dad8f11d9f90a36c76bf66
Usejun <84970153+Usejun@users.noreply.github.com>
2023-09-04 23:14:09 +0900

    Create Program.cs

---
+++ 11051번/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Boj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = input[0], m = input[1], k = n - m;
+            BigInteger answer = 1;
+
+            while (n > 1)
+            {
+                answer *= n--;
+                while (m > 1 && answer % m == 0) answer /= m--;
+                while (k > 1 && answer % k == 0) answer /= k--;
+            }
+
+            Console.WriteLine(answer % 1007);
+        }
+    }
+}
