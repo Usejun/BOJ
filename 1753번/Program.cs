533f7801c4c056e163fdc16742931a2160793e19
Usejun <84970153+Usejun@users.noreply.github.com>
2023-09-17 20:13:19 +0900

    Update Program.cs

--- 1753번/Program.cs
+++ 1753번/Program.cs
@@ -12,33 +12,36 @@ namespace Boj
     {     
         static void Main(string[] args)            
         {
-            var sb = new System.Text.StringBuilder();
             var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
             int n = input[0], m = input[1], INF = int.MaxValue;
             int S = int.Parse(Console.ReadLine());
-            var graph = Enumerable.Range(0, n + 1).Select(i => new List<Tuple<int, int>>()).ToArray();
-            var pq = new PriorityQueue<Tuple<int, int>, int>();
+            var graph = Enumerable.Range(0, n + 1).Select(i => new List<(int, int)>()).ToArray();
+            var pq = new SortedSet<(int, int)>();
             var dis = Enumerable.Repeat(INF, n + 1).ToArray();
-
+            
             for (int i = 0; i < m; i++)
             {
                 input = Console.ReadLine().Split().Select(int.Parse).ToArray();
-                graph[input[0]].Add(new Tuple<int, int>(input[1], input[2]));
+                graph[input[0]].Add((input[2], input[1]));
             }
-
-            pq.Enqueue(new Tuple<int, int>(S, 0), 0);
-
+            
+            pq.Add((0, S));
+            dis[S] = 0;
+            
             while (pq.Count != 0)
             {
-                (int node, int wei) = pq.Dequeue();
-                if (dis[node] != INF) continue;
-                dis[node] = wei;
-                foreach ((int newNode, int newWei) in graph[node])
-                    pq.Enqueue(new Tuple<int, int>(newNode, wei + newWei), wei + newWei);
+                (int wei, int node) = pq.Min;
+                pq.Remove(pq.Min);
+                foreach ((int nextWei, int next) in graph[node])
+                    if (dis[next] > dis[node] + nextWei)
+                    {
+                        dis[next] = dis[node] + nextWei;
+                        pq.Add((wei + nextWei, next));
+                    }                
             }
-
-            for (int i = 1; i < n + 1; i++)
-                Console.WriteLine(dis[i] == INF ? "INF" : dis[i].ToString());      
+            
+            foreach (int i in dis.Skip(1))
+                Console.WriteLine(i == INF ? "INF" : i.ToString());
         }
     }
 }
