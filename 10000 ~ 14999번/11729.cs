e98543f2152792929edd908ee1317b4ddb7d88fc
Usejun <84970153+Usejun@users.noreply.github.com>
2023-11-28 10:33:35 +0900

    11729번 하노이 탑 이동 순서

--- 10000 ~ 14999번/11729.cs
+++ 10000 ~ 14999번/11729.cs
@@ -1,1 +1,36 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
 
+namespace Boj
+{
+    internal class Program
+    {     
+        static void Main(string[] args)            
+        {
+            var sb = new System.Text.StringBuilder();
+            int n = int.Parse(Console.ReadLine());
+            sb.Append((1<<n) - 1).Append('\n');
+            hanoi(n, 1, 2, 3);
+            Console.Write(sb);
+            
+            void hanoi(int N, int start, int sub, int end)
+            {
+                if (N == 1)
+                {
+                    sb.AppendLine($"{start} {end}");
+                }
+                else
+                {
+                    hanoi(N - 1, start, end, sub);
+                    sb.AppendLine($"{start} {end}");
+                    hanoi(N - 1, sub, start, end);
+                }
+            }
+        }
+    }
+}
