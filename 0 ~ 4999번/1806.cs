728319040a186394b9055f8c7b328346cefe8355
Usejun <84970153+Usejun@users.noreply.github.com>
2023-11-28 10:30:32 +0900

    1806번 부분합

--- 0 ~ 4999번/1806.cs
+++ 0 ~ 4999번/1806.cs
@@ -16,35 +16,23 @@ namespace Boj
             int n = input[0], m = input[1];
             var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            int i = 0, j = 0, s = 0, l = 0;
+            int i = 0, j = 0, s = array[0], l = 0;
             int max = int.MaxValue;
 
             while (j < n && i < n)
             {
                 if (s < m)
                 {
-                    s += array[i++];
-                    l++;
+                    i++;
+                    if (i >= n) break;
+                    s += array[i];
                 }
                 else
                 {
-                    max = Math.Min(max, l);
-                    s -= array[j++];
-                    l--;
+                    max = Math.Min(max, i - j + 1);
+                    s -= array[j++];                 
                 }                  
             }
-
-            if (s >= m)
-                max = Math.Min(max, l);
-
-            while (j < n)
-            {
-                s -= array[j++];
-                l--;
-                if (s >= m)
-                    max = Math.Min(max, l);
-            }
-             
 
             Console.WriteLine(max == int.MaxValue ? 0 : max);
         }
