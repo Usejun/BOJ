5f1f4ea8c5170c52ee7abd829689d9b0da50433a
Usejun <84970153+Usejun@users.noreply.github.com>
2023-12-22 15:17:07 +0900

    14606번 피자 (Small)

---
+++ 10000 ~ 14999번/14606.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Boj
+{
+    internal class Program
+    {             
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.Write(n * (n - 1) / 2);
+        }
+    }
+}
