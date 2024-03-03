6722d3995b9615d3ed53a80a170dd1f3c3c7d0e0
Usejun <84970153+Usejun@users.noreply.github.com>
2024-03-03 18:39:14 +0900

    11441번 합 구하기

---
+++ 10000 ~ 14999번/11441.cs
@@ -0,0 +1,14 @@
+int n = int.Parse(Console.ReadLine());
+var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+var s = new int[n + 1];
+
+for (int i = 1; i <= n; i++)
+    s[i] = s[i - 1] + a[i - 1];
+
+int k = int.Parse(Console.ReadLine());
+while (k-- > 0)
+{
+    var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    int i = input[0], j = input[1];
+    Console.WriteLine(s[j] - s[i - 1]);
+}
